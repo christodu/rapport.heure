4005c5c4849870fde27b6ca6f15eaa18ca393e63
christodu <christopher.chokbeton@gmail.com>
2026-09-03 16:54:51 +0200

    Update cache version in service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "chok-beton-v1";
+const CACHE = "chok-beton-v2-google";
 const ASSETS = ["./index.html", "./icon-192.png", "./icon-512.png", "./manifest.json"];
 
 self.addEventListener("install", e => {
