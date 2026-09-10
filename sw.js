b33382d71a837dfc14cbb6943ee56277705276d0
christodu <christopher.chokbeton@gmail.com>
2026-09-10 09:16:48 +0200

    Increment service worker version to v42

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
 //     incrémentez le numéro ci-dessous (v3, v4, …).
 //     C'est ce qui force les téléphones à purger l'ancien cache.
 // ============================================================
-const VERSION = "v41";
+const VERSION = "v42";
 const CACHE = `chok-beton-${VERSION}`;
 const ASSETS = ["./index.html", "./icon-192.png", "./icon-512.png", "./manifest.json"];
 
